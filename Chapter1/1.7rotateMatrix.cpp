8900fae8e6a7fec0945189818abda90802d3fec2
Lei Xu <daniel_xulei@hotmail.com>
2018-08-14 21:03:46 -0700

    1.7 Rotate Matrix

--- Chapter1/1.7rotateMatrix.cpp
+++ Chapter1/1.7rotateMatrix.cpp
@@ -4,9 +4,24 @@
 using namespace std;
 
 void rotateMatrix(vector<vector<int> >& matrix) {
+	int n = matrix.size();
+	for(int layer =0; layer<n;layer++){
+		int first = layer;
+		int last = n-1-layer;
+		for(int i=first;i<last;i++){
+			int offset = i-first;
+			int top = matrix[first][i];
 
+			matrix[first][i] = matrix[last-offset][first];
 
+			matrix[last-offset][first] = matrix[last][last-offset];
 
+			matrix[last][last-offset] = matrix[i][last];
+
+			matrix[i][last] = top;
+			
+		}
+	}
 
 }
 
